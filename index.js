762654fdd9b0bc24c10ba3943f81b3eae442663f
Robert Stepanov <robertdstepanov@gmail.com>
2020-02-13 14:01:41 -0500

    secret

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 
 const server = express();
@@ -15,3 +17,4 @@ const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port}`))
 
 // npm i -D nodemon
+// npx gitignore node
